d2ef3461a3a903a5e1f7fbec39cbb32e4550f27f
SKINPALL <max.095214@gmail.com>
2026-01-14 12:12:27 +0700

    update

--- lists/views.py
+++ lists/views.py
@@ -5,12 +5,18 @@ from lists.models import Item
 # Create your views here.
 
 def home_page(request):
-    if request.method == "POST":
-        Item.objects.create(text=request.POST["item_text"])
-        return redirect("/")
-
-    items = Item.objects.all()
-    return render(request, "home.html", {"items": items})
+    
+    return render(request, "home.html")
 
 def about_page(request) :
     return render(request , "about.html")
+
+def new_list(request):
+    Item.objects.create(text=request.POST["item_text"])
+    return redirect("/lists/the-only-list-in-the-world/")
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, "list.html", {"items": items})
+
+    
--- superlists/urls.py
+++ superlists/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from lists.views import home_page
 from lists.views import about_page
+from lists import views
 
 urlpatterns = [
     path("", home_page, name="home"),
     path("about/", about_page, name="about"),
+    path("lists/new", views.new_list, name="new_list"),
+    path("lists/the-only-list-in-the-world/", views.view_list, name="view_list"), 
 ]
